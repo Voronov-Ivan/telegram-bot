a79cf9ee35423f4478b7df67c0c9ee8cfa0e9690
Ivan Voronov <vanya.voronov.25062002@gmail.com>
2021-06-27 22:26:06 +0300

    make it better

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+
+namespace UrlSendBot
+{
+    class Program
+    {
+        private static string token {get;set;} = "1865373257:AAFPMQMrC_wgiuRAYBeU-zFgFeEqoaiIO70";
+        private static TelegramBotClient client;
+        static void Main(string[] args)
+        {
+            client = new TelegramBotClient(token);
+            client.StartReceiving();
+            client.OnMessage += OnBotMAssage;
+            Console.ReadLine();
+            client.StartReceiving();
+        }
+
+        private static void OnBotMAssage(object sender, MessageEventArgs e)
+        {
+            var msg = e.Message;
+            if (msg.Text != null)
+            {
+                Console.WriteLine(msg.Text);
+            }
+        }
+    }
+}
